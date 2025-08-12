ec5bbf95af990ab0d43addfd28c744d23164fe2e
akodm <a8456452@naver.com>
2025-08-12 14:01:39 +0900

    ga-stub

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 import { GoogleAnalytics } from '@next/third-parties/google';
 import { Geist, Geist_Mono } from 'next/font/google';
 import '@/app/globals.css';
+import Script from 'next/script';
 
 const { NEXT_PUBLIC_GA_TAG = '' } = process.env;
 
@@ -33,6 +34,14 @@ export default function RootLayout({
       >
         {children}
         <SpeedInsights />
+        <Script id="ga-stub" strategy="beforeInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${NEXT_PUBLIC_GA_TAG}');
+          `}
+        </Script>
       </body>
       <GoogleAnalytics gaId={NEXT_PUBLIC_GA_TAG} />
     </html>
