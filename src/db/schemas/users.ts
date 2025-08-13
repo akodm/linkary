5a50c90f068b8c4d4163ac78feb8bd2ef9bde78d
akodm <a8456452@naver.com>
2025-08-13 14:44:35 +0900

    user 모델 수정

--- src/db/schemas/users.ts
+++ src/db/schemas/users.ts
@@ -1,5 +1,5 @@
-import { relations } from 'drizzle-orm';
-import { pgTable, serial, timestamp } from 'drizzle-orm/pg-core';
+import { relations, sql } from 'drizzle-orm';
+import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 import { userApi } from '@/db/schemas/userApi';
 import { linkFolder } from '@/db/schemas/linkFolder';
 import { link } from '@/db/schemas/link';
@@ -7,7 +7,16 @@ import { linkView } from '@/db/schemas/linkView';
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
+  role: text('role').notNull().default('user'),
+  name: text('name').notNull().default(''),
+  slug: text('slug')
+    .notNull()
+    .unique()
+    .default(sql`gen_random_uuid()`),
+  provider: text('provider').notNull().default(''),
+  providerId: text('provider_id').notNull().default(''),
   createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
 });
 
 export const usersRelations = relations(users, ({ many }) => ({
