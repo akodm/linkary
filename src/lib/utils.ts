e8e7842136cad31ce66afcb80bad455cd7c67b90
akodm <a8456452@naver.com>
2025-08-13 14:58:07 +0900

    name generator

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import Haikunator, { Config } from 'haikunator';
+
+const h = new Haikunator();
+const defaultOptions: Config = { delimiter: '-', tokenLength: 4 };
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function generateName(options: Config = defaultOptions) {
+  const name = h.haikunate(options);
+
+  return name;
 }
