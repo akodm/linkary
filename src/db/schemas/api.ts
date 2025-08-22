6098847a037e6aafd7fcd4f829cdb0307667bab4
akodm <a8456452@naver.com>
2025-08-22 16:01:23 +0900

    feat: 주석 추가 및 칼럼 추가

--- src/db/schemas/api.ts
+++ src/db/schemas/api.ts
@@ -4,10 +4,12 @@ import { userApi } from '@/db/schemas/userApi';
 
 export const api = pgTable('api', {
   id: serial('id').primaryKey(),
-  name: text('name').notNull().default(''),
-  usage: integer('usage').notNull().default(0),
-  limit: integer('limit').notNull().default(1000),
-  enterprise: boolean('enterprise').notNull().default(false),
+  name: text('name').notNull().default(''), // Google Web Risk API, Tavily 등
+  usage: integer('usage').notNull().default(0), // 현재까지 전체 사용량 (사용량 초기화 시 0으로 초기화)
+  limit: integer('limit').default(-1), // -1 은 무제한
+  enterprise: boolean('enterprise').notNull().default(false), // 결제 여부 (결제된 API 는 limit 가 필요 없음)
+  initialDateType: text('initial_date_type').notNull().default('month'), // 초기화되는 날짜 (일, 월, 연 단위 등)
+  initialDateValue: integer('initial_date_value'), // 초기화되는 정확한 일자 (매월 1일 초기화 = type: month, value: 1 등)
 });
 
 export const apiRelations = relations(api, ({ many }) => ({
