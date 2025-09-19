62d5cb1901a12e8d1a288d8bca2c3a54f05ddb34
akodm <a8456452@naver.com>
2025-09-19 18:02:37 +0900

    feat: 로컬 스토리지 키

---
+++ src/consts/keys.ts
@@ -0,0 +1,6 @@
+export const LANGUAGE_KEY = 'linkary-language';
+
+export const LOCAL_USER_KEY = 'linkary-guest-user';
+export const LOCAL_FOLDER_KEY = 'linkary-guest-folder';
+export const LOCAL_LINK_LISTS_KEY = 'linkary-guest-link-lists';
+export const LOCAL_LINKP_VIEWER_KEY = 'linkary-guest-link-viewer';
