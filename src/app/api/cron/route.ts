490d452df330651a5f0446b2393506decc4dcb80
akodm <a8456452@naver.com>
2025-12-09 17:13:05 +0900

    feat: 월 쿼리 크론 작업 추가

---
+++ src/app/api/cron/route.ts
@@ -0,0 +1,40 @@
+import { db } from '@/db';
+import { api } from '@/db/schemas/api';
+import { userApi } from '@/db/schemas/userApi';
+import { sentryCaptureException } from '@/lib/utils';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  try {
+    if (
+      request.headers.get('Authorization') !==
+      `Bearer ${process.env.CRON_SECRET}`
+    ) {
+      return NextResponse.json(
+        { data: null, message: 'Unauthorized' },
+        { status: 401 },
+      );
+    }
+
+    await db.transaction(async (tx) => {
+      await tx.update(api).set({ usage: 0 });
+      await tx.update(userApi).set({ usage: 0 });
+    });
+
+    return NextResponse.json(
+      { data: true, message: 'Cron job completed successfully' },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error(error);
+
+    sentryCaptureException(error, 'cron', {
+      message: 'Cron job failed',
+    });
+
+    return NextResponse.json(
+      { data: false, message: 'Cron job failed' },
+      { status: 500 },
+    );
+  }
+}
