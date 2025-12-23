642dfd8d818a89722c62950849f90695f2596681
akodm <a8456452@naver.com>
2025-12-23 16:23:18 +0900

    fix: 레이아웃 버그 수정

--- src/components/community/Layout.tsx
+++ src/components/community/Layout.tsx
@@ -5,6 +5,7 @@ import { LinkAndFolderCommunityResponse } from '@/lib/actions/link';
 import Banner from '@/components/community/Banner';
 import Pagination from 'src/components/community/Pagination';
 import Content from 'src/components/community/Content';
+import clsx from 'clsx';
 
 interface CommunityLayoutProps {
   links: LinkAndFolderCommunityResponse['data'];
@@ -13,7 +14,12 @@ interface CommunityLayoutProps {
 
 export default function CommunityLayout(props: CommunityLayoutProps) {
   return (
-    <div className="flex flex-col items-center w-full h-full relative">
+    <div
+      className={clsx(
+        'flex flex-col items-center w-full relative',
+        props.total === 0 ? 'h-full' : 'h-auto',
+      )}
+    >
       <Header isLogo isLanguage isSignIn />
       <MainProvider>
         <Banner />
