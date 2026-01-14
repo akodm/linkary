2d48d8742cf6f98e5bd7ee4bf5d75795f9221621
akodm <a8456452@naver.com>
2026-01-14 14:56:36 +0900

    feat: 제작자 제거

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -8,11 +8,10 @@ export default function Footer() {
 
   return (
     <footer className="flex flex-col items-start md:items-center gap-3 md:gap-5 w-full px-5 py-10 border-t border-border bg-blue-500">
-      <div className="flex flex-col md:flex-row items-start md:items-center gap-3 md:gap-5 w-full md:w-fit text-left">
+      <div className="flex flex-col md:flex-row items-center gap-3 md:gap-5 w-full md:w-fit text-left">
         <span className="mx-auto md:mx-0 text-xs md:text-sm text-gray-300">
           {i18n.t('© 2025 Linkary. All rights reserved.')}
         </span>
-
         <div className="flex flex-row items-center gap-x-1.5">
           <ContactIcon size={14} className="text-white" />
           <a
@@ -22,20 +21,6 @@ export default function Footer() {
             {i18n.t('Contact Us')}
           </a>
         </div>
-      </div>
-      <div className="flex flex-row flex-wrap items-center gap-x-1.5 mx-auto">
-        <span className="text-xs text-gray-300">
-          {i18n.t('Site created by 🤖 akodm')}
-        </span>
-        <a
-          href="https://github.com/akodm"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="underline text-xs md:text-sm text-white hover:text-gray-200 transition-colors duration-200">
-            {'https://github.com/akodm'}
-          </span>
-        </a>
       </div>
     </footer>
   );
